de022fcf73c1c8e24f7f36465ac0da19fdc4e4a4
nideez45 <75631297+nideez45@users.noreply.github.com>
2022-07-21 09:00:20 +0530

    Add files via upload

---
+++ output.py
@@ -0,0 +1,48 @@
+import maintaining_arc_consistency as bs_mac
+import input
+
+rows = input.rows
+colms = input.colums
+answer = bs_mac.answer
+
+output_matrix_vertical = input.matrix_vertical
+output_matrix_horizontal = input.matrix_horizonal
+
+# making the output matrix
+
+for i in range(rows):
+    for j in range(colms):
+        if(output_matrix_vertical[i][j]=='0'):
+            output_matrix_vertical[i][j] = answer[(i,j)]
+            
+for i in range(rows):
+    for j in range(colms):
+        if(output_matrix_horizontal[i][j]=='0'):
+            output_matrix_horizontal[i][j] = answer[(i,j)]
+
+# making output file
+
+with open('output7.txt', 'w') as f:
+    f.write("rows={}\n".format(rows))
+    f.write("columns={}\n".format(colms))
+    f.write("Horizontal\n",)
+    for lists in output_matrix_horizontal:
+        cnt=0
+        for item in lists:
+            cnt+=1
+            if(cnt==colms):
+                f.write("%s" % item)
+            else:    
+                f.write("%s," % item)
+        f.write("\n")
+        
+    f.write("Vertical\n",)
+    for lists in output_matrix_vertical:
+        cnt=0
+        for item in lists:
+            cnt+=1
+            if(cnt==colms):
+                f.write("%s" % item)
+            else:    
+                f.write("%s," % item)
+        f.write("\n")
